d55bf3e60e0087ed1612ddac5a5d020b4570bf1b
mtarnowski <michal.tarnowski03@gmail.com>
2024-08-30 00:52:05 +0200

    header finished, footer +-

---
+++ script.js
@@ -0,0 +1,23 @@
+const header = document.getElementById("header-container");
+
+header.addEventListener("mouseover", () => {
+    header.style.opacity = "1";
+});
+
+header.addEventListener("mouseout", () => {
+    const scrollValue = document.documentElement.scrollTop;
+    const opacityValue = 1 - (scrollValue * 0.001);
+    header.style.opacity = opacityValue.toString();
+});
+
+window.onscroll = () => {
+    if (document.documentElement.scrollTop <= 50) {
+        header.style.opacity = "1";
+    } else {
+        const scrollValue = document.documentElement.scrollTop;
+        const opacityValue = 1 - (scrollValue * 0.001);
+        header.style.opacity = opacityValue.toString();
+    }
+};
+
+
